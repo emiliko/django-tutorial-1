24518f4dd435ae28c80a54e12a102ff935f1cef7
Emilia <emilia.likolahti@gmail.com>
2020-09-30 17:17:18 +0300

    Andd another test

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.urls import resolve
+from .views import home
 
 class HomeTests(TestCase):
 
@@ -10,3 +12,10 @@ class HomeTests(TestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200) 
 
+    # Django uses resolve function to match a requested URL with a list
+    # of URLS listed in the urls.py module.
+    # The test will make sure the URL /, which is the root URL,
+    # is returning the home view. 
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
