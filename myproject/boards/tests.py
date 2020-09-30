5bf2f73bdec9e2333c2e8bd261ce34369a7d8213
Emilia <emilia.likolahti@gmail.com>
2020-09-30 17:06:03 +0300

    Add first test

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeTests(TestCase):
+
+    # test the status code of the response
+    # status code 200 means success
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200) 
+
