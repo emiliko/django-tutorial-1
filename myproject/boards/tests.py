fd44ec2e6a514b62a58e8fa63851af4b1541f983
Emilia <emilia.likolahti@gmail.com>
2020-10-03 17:22:35 +0300

    Implement a form wrong way

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
-from .views import home, board_topics
-from .models import Board
+from .views import home, board_topics, new_topic
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
+
 
 class HomeTests(TestCase):
 
@@ -56,9 +58,88 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
-    
-    def test_board_topics_view_contains_link_back_to_homepage(self):
+
+    def test_board_topics_view_contains_navigation_link(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+class NewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+        
+        # create user instance to use in tests
+        User.objects.create_user(username='john', email='john@doe.com', password='1234')
+    
+    # check if the request to the view is succesful
+    def test_new_topic_view_success_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # check if the view is raising a 4040 error when the Board does not exist
+    def test_new_topic_view_not_found_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    # check if the right view is being used
+    def test_new_topic_url_resolves_new_topic_view(self):
+        view = resolve('/boards/1/new/')
+        self.assertEquals(view.func, new_topic)
+    
+    #ensure the navigation back to the list of topics
+    def test_new_topic_view_contains_link_back_to_board_topics_view(self):
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    # ensure HTML containts CSRF token
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+    
+    # sends a valid combination of data and check if the view created
+    # a Topic instance and a Post instance
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Loren ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+    
+    # send an empty dictionary to check how the application is behaving
+    def test_new_topic_invalid_post_data(self):
+        # Invalid post data should not redirect
+        # The expected behavior is to show the form again with validation errors
+
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+    
+    # similar to test above, but notw sending some date
+    # The application is expected to validate and reject empty subject and message
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        #Invalid post data should not redirect
+        # The expected behavior is to show the form again with validation errors
+
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post,objects.exists())
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,13 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import url
-
-
 from boards import views
 
 urlpatterns = [
     url(r"^$", views.home, name="home"),
     re_path("boards/(?P<pk>\d+)/$", views.board_topics, name="board_topics"),
+    re_path("boards/(?P<pk>\d+)/new/$", views.new_topic, name="new_topic"),
     url(r"^admin/", admin.site.urls),
 
 ]    
